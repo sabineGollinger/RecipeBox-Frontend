f206b7a48e033ba68ed303d4c0fa522d3b47e07b
michistangl <michaela.stangl@edu.fh-joanneum.at>
2018-01-14 20:03:58 +0100

    recipe added

---
+++ src/app/recipe-search/recipe-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'recipe-search',
+  templateUrl: './recipe-search.component.html',
+  styleUrls: ['./recipe-search.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RecipeSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
