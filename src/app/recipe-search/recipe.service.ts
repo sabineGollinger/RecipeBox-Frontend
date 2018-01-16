079cf98e55b8c6e1bb00fe956981891b0f1d19e1
michistangl <michaela.stangl@edu.fh-joanneum.at>
2018-01-16 09:06:59 +0100

    recipe changes

--- src/app/recipe-search/recipe.service.ts
+++ src/app/recipe-search/recipe.service.ts
@@ -1,6 +1,5 @@
 import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Injectable} from "@angular/core";
-import {Flight} from "../../entities/flight";
 import {Observable} from "rxjs/Observable";
 import {Recipe} from "../entities/recipe";
 
@@ -9,16 +8,16 @@ export class RecipeService {
   constructor(private http: HttpClient) {
   }
   find(name: string, category: string): Observable<Recipe[]> {
-    //let url = 'http://www.angular.at/api/flight';
-    let url = '!!!! --- TO BE IMPLEMENTED --- !!!!';                               // IMPLEMENTIERUNG!!!!
+    let url = 'http://www.angular.at/api/flight';
+    //let url = '!!!! --- TO BE IMPLEMENTED --- !!!!';                               // IMPLEMENTIERUNG!!!!
     let headers = new HttpHeaders() .set('Accept', 'application/json');
     let params = new HttpParams() .set('name', name) .set('category', category);
     return this.http.get<Recipe[]>(url, {headers, params})
   }
 
   findById(id: string): Observable<Recipe> {
-   // const url = 'http://www.angular.at/api/flight';
-    const url = '!!!! --- TO BE IMPLEMENTED --- !!!!';                            // IMPLEMENTIERUNG!!!!
+    const url = 'http://www.angular.at/api/flight';
+    //const url = '!!!! --- TO BE IMPLEMENTED --- !!!!';                            // IMPLEMENTIERUNG!!!!
     const params = new HttpParams()
       .set('id', id);
     const headers = new HttpHeaders()
@@ -28,8 +27,8 @@ export class RecipeService {
 
 
   save(r: Recipe): Observable<Recipe> {
-    //const url = 'http://www.angular.at/api/flight';
-    const url = '!!!! --- TO BE IMPLEMENTED --- !!!!';                            // IMPLEMENTIERUNG!!!!
+    const url = 'http://www.angular.at/api/flight';
+    //const url = '!!!! --- TO BE IMPLEMENTED --- !!!!';                            // IMPLEMENTIERUNG!!!!
     const headers = new HttpHeaders()
       .set('Accept', 'application/json');
     return this.http.post<Recipe>(url, r, { headers});
