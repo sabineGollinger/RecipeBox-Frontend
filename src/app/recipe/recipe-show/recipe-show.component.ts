d9fe5f8884a1a0122450b360d9691731fc571348
michistangl <michaela.stangl@edu.fh-joanneum.at>
2018-01-22 17:28:27 +0100

    some changes

--- src/app/recipe/recipe-show/recipe-show.component.ts
+++ src/app/recipe/recipe-show/recipe-show.component.ts
@@ -5,6 +5,7 @@ import {RecipeService} from '../recipe-search/recipe.service';
 import {PdfmakeService} from '../pdfmake/pdfmake.service';
 import {User} from "../../entities/user";
 import {AuthService} from "../../shared/auth/auth.service";
+import * as pdfMake from 'pdfmake/build/pdfmake';
 
 
 @Component({
@@ -24,6 +25,8 @@ export class RecipeShowComponent { // implements OnInit {
   loginUser: User;
   loginUserRole: string;
   username: string;
+
+  pdfContent =  {};
 
   constructor(
     private authService: AuthService,
@@ -45,30 +48,31 @@ export class RecipeShowComponent { // implements OnInit {
             console.log(this.recipe);
 
             // Configure PDF
-              this.pdfmake.configureStyles({
-                h1: {fontSize: 18, bold: true},
-                h2: {fontSize: 14, bold: true},
-                info: {fontSize: 12, color: '#71777C'}
-              });
-              this.pdfmake.addText(this.recipe.name, 'h1');
-              this.pdfmake.addText(this.recipe.category, 'h2');
-              this.pdfmake.addText('\n\n');
-              this.pdfmake.addText('Zutaten', 'h2');
-              this.pdfmake.addText('\n');
-              this.pdfmake.addText(this.recipe.ingredient);
-              this.pdfmake.addText('\n\n');
-              this.pdfmake.addText('Zubereitung', 'h2');
-              this.pdfmake.addText('\n');
-              this.pdfmake.addText(this.recipe.preparation);
-              this.pdfmake.addText('\n\n');
-              this.pdfmake.addText('Hinweise', 'h2');
-              this.pdfmake.addText('\n');
-              this.pdfmake.addText(this.recipe.hint);
-              this.pdfmake.addText('\n\n\n');
-              this.pdfmake.addText('Rezept heruntergeladen von', 'info');
-              this.pdfmake.addText('\n');
-              this.pdfmake.addImage('./assets/img/RecipeBox Logo.gif', 150);
-
+            this.pdfContent =  {
+              content: [this.pdfmake.configureStyles({
+              h1: {fontSize: 18, bold: true},
+              h2: {fontSize: 14, bold: true},
+              info: {fontSize: 12, color: '#71777C'}
+            }),
+            this.pdfmake.addText(this.recipe.name, 'h1'),
+            this.pdfmake.addText(this.recipe.category, 'h2'),
+            this.pdfmake.addText('\n\n'),
+            this.pdfmake.addText('Zutaten', 'h2'),
+            this.pdfmake.addText('\n'),
+            this.pdfmake.addText(this.recipe.ingredient),
+            this.pdfmake.addText('\n\n'),
+            this.pdfmake.addText('Zubereitung', 'h2'),
+            this.pdfmake.addText('\n'),
+            this.pdfmake.addText(this.recipe.preparation),
+            this.pdfmake.addText('\n\n'),
+            this.pdfmake.addText('Hinweise', 'h2'),
+            this.pdfmake.addText('\n'),
+            this.pdfmake.addText(this.recipe.hint),
+            this.pdfmake.addText('\n\n\n'),
+            this.pdfmake.addText('Rezept heruntergeladen von', 'info'),
+            this.pdfmake.addText('\n'),
+            this.pdfmake.addImage('./assets/img/RecipeBox Logo.gif', 150)] };
+            pdfMake.createPdf(this.pdfContent);
           },
           err => {this.errors = 'Fehler beim Laden'; }
         );
